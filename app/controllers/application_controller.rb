c31e0d39e27c7ed0a1529913ea211df71ac1f032
shin-ito0306 <shin.20000306@icloud.com>
2021-05-11 07:37:56 +0000

    [add] after_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permited_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "ログイン成功"
+      user_path(current_user.id)
+    else
+      flash[:notice] = "新規登録"
+      user_path(current_user.id)
+    end
+  end
+
   protected
 
   def configure_permited_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
