2be4563f67c32a7db36a9e4f2e3ced35a688b4a0
shin-ito0306 <shin.20000306@icloud.com>
2021-05-12 09:20:32 +0000

    [add] user/update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a62bf516fbc0ceedc240d8cf4abf01f27252dc0d63393006ca32f73d74b21876367779864ee90f4d84d7a0bc4830099a1aeeef76c03e480715c10f555ed94282'
