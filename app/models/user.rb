f13745e98b13ae29d47a21d19e64e3851834b8ef
shin-ito0306 <shin.20000306@icloud.com>
2021-06-04 04:18:04 +0000

    [add]model,controller,index

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,22 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
-  has_many :follower, class_name: "Relationship", foregin_key: "follower_id", dependent: :destroy
-  has_many :followed, through: :follower, source: :follower
-  
-  has_many :followed, class_name: "Relatiionship", foregin_key: "followed_id", dependent: :destroy
-  has_many :follower, through: :followed, source: :follower
-  
+
+  has_many :follower_do, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :follower_user, through: :follower_do, source: :followed
+
+  has_many :followed_do, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followed_user, through: :followed_do, source: :follower
+
   attachment :profile_image
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
+
+  def follow_by?(user)
+    followed_do.find_by(follower_id: user.id).present?
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+  end
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
