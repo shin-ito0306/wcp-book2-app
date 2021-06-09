2b90d86495e63f4d30977c9451bc0a5267f20479
shin-ito0306 <shin.20000306@icloud.com>
2021-06-09 04:51:32 +0000

    [add]message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_044903) do
+ActiveRecord::Schema.define(version: 2021_06_09_045107) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2021_06_09_044903) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "text"
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
