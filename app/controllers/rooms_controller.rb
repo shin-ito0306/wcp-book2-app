927e19cce7e71194bb2f397a2201fa3726c01cf7
shin-ito0306 <shin.20000306@icloud.com>
2021-06-09 06:29:19 +0000

    [add]rooms_controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,26 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.create
+    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
+    @entry2 = Entry.create(entry_params)
+    redirect_to room_path(@room.id)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+    def entry_params
+      params.require(:entry).permit(:room_id, :user_id).marge(room_id: @room.id)
+    end
 end
