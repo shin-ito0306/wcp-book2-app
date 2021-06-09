36d841e417380186be2f0f76d0521caf3266d0bd
shin-ito0306 <shin.20000306@icloud.com>
2021-06-09 09:58:03 +0000

    [finish] app-issues8a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,7 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-    else
-      flash[:alert] = "メッセージの送信に失敗しました。"
     end
-    redirect_to room_path(@message.room_id)
   end
 
   private
